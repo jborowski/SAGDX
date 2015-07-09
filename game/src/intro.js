56cba9e8e920962eaf43e060a477b08faf495352
GlyphGryph <glyphgryph@gmail.com>
2015-07-09 19:53:16 -0400

    Something Stupid

--- game/src/intro.js
+++ game/src/intro.js
@@ -1,8 +1,10 @@
 var introState = {
   jumpSpeed: 15,
   playerSpeed: 15,
-  jumpFinished: true,
-  jumpTimer: 0,
+  maxJumpHeight: 4*gridSize,
+  jumping: false,
+  jumpStart: 0,
+  jumpHeight: 0,
   debug:false,
   preload: function(){
     this.game.load.tilemap('map', 'data/tiles_map.json', null, Phaser.Tilemap.TILED_JSON);
@@ -40,7 +42,7 @@ var introState = {
     this.player.body.collideWorldBounds = true;
     this.game.camera.follow(this.player);
     if(this.debug){
-      this.debugText = this.game.add.text(5, 5, 'DEBUG INFO ', { fontSize: '8px', fill: '#000' });
+      this.debugText = this.game.add.text(5, 50, 'DEBUG INFO ', { fontSize: '8px', fill: '#000' });
     }
   },
   update: function(){
@@ -49,41 +51,40 @@ var introState = {
 
     if(this.debug){
       this.debugText.text = "DEBUG INFO - Player Info: [X:"+this.player.x+"] [Y:"+this.player.y+"]\n"+
-          "[JUMPTIMER:"+this.jumpTimer+"] [JUMPFINISHED:"+this.jumpFinished+"]";
+          "[JUMP HEIGHT:"+this.jumpHeight+"] [JUMPING:"+this.jumping+"] [ON FLOOR:"+this.player.body.onFloor()+"]";
     }
   },
   updatePlayer: function(){
     this.player.body.velocity.x = 0;
 
-    if (this.player.body.onFloor() && this.cursors.up.isDown && this.jumpFinished == true) {
-      this.player.body.velocity.y = gridSize*-this.jumpSpeed;
-      this.jumpFinished = false;
-      this.jumpTimer = 0;
-    }
-
-    if (this.cursors.left.isDown){
+    // Horizontal Movement
+    if(this.cursors.left.isDown){
       this.player.body.velocity.x = gridSize*-this.playerSpeed;
     }
-    else if (this.cursors.right.isDown){
+    else if(this.cursors.right.isDown){
       this.player.body.velocity.x = gridSize*this.playerSpeed;
     }
 
-    if (!this.player.body.onFloor()){
-      this.player.body.velocity.y += gridSize*1.2;
-      this.jumpTimer += this.game.time.physicsElapsed;
+    // Jumps
+    if(this.player.body.onFloor() && this.cursors.up.isDown) {
+      this.jumping = true;
+      this.jumpStart = this.player.body.y;
     }
-
-    if (this.cursors.up.isDown && !this.player.body.onFloor() && this.jumpTimer < 0.2){
-      this.player.body.velocity.y += gridSize*-1.3;
+    
+    if(this.jumping){
+      this.jumpHeight = this.jumpStart - this.player.body.y;
+      if(this.cursors.up.isDown && !this.player.body.blocked.up && this.jumpHeight <= this.maxJumpHeight){
+        this.player.body.velocity.y += gridSize*-(1.1-this.jumpHeight/this.maxJumpHeight);
+      } else {
+        this.jumping = false;
+        this.jumpingFor = 0;
+        this.player.body.velocity.y = 0;
+      }
+    } else {
+      this.player.body.gravity.y = gridSize*20;
     }
-
-    if (this.player.body.velocity.y > gridSize*15){
+    if(this.player.body.velocity.y > gridSize*15){
       this.player.body.velocity.y = gridSize*15;
-    }
-
-    if (this.player.body.blocked.down && this.cursors.up.isUp){
-      this.jumpFinished = true;
-      this.player.body.velocity.y = 0;
     }
   }
 }
