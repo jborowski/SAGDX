1cfb6e87f13c3aa8ebeac94190cc3009d877b659
GlyphGryph <glyphgryph@gmail.com>
2015-07-31 21:10:50 -0400

    And back to intro

--- game/src/Preloader.js
+++ game/src/Preloader.js
@@ -104,8 +104,8 @@ SAGDX.Preloader.prototype = {
   update: function () {
     if (this.cache.isSoundDecoded('music') && this.ready == false) {
       this.ready = true;
-      //this.state.start('Intro');
-      this.state.start('Level2');
+      this.state.start('Intro');
+      //this.state.start('Level2');
     }
   }
 };
